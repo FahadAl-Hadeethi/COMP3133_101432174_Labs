a02f1ca6039f47de3976f72a9ba8f139fdc26eb9
Fahad <fahedihssan@gmail.com>
2025-03-08 21:42:49 -0500

    LabEx6 work

---
+++ Lab06_101432174/exercise-4/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var customer_1 = require("./customer");
+var customer1 = new customer_1.Customer("Fahad", "Al-Hadeethi", 24);
+customer1.greeter();
+customer1.getAge();
---
+++ Lab06_101432174/exercise-4/main.ts
@@ -0,0 +1,5 @@
+import { Customer } from "./customer";
+
+const customer1 = new Customer("Fahad", "Al-Hadeethi", 24);
+customer1.greeter();
+customer1.getAge();
